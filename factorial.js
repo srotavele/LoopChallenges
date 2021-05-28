cdbcdb12c4af4db07a742f9eac6df99b80df8351
MRea <rotavele666@pm.me>
2021-05-27 20:17:00 -0700

    Final Commit

---
+++ factorial.js
@@ -0,0 +1,5 @@
+var product = 1;
+for(i=1; i<=12; i++){
+product = product * i;
+console.log(product);
+}
--- sigma.js
+++ sigma.js
@@ -1,3 +1,10 @@
-var sum
-for( i = 1)
+function sum(n){
+var sum = 0
+for( i=0; i<=n; i++){
+    sum += i
 }
+return sum
+}
+console.log(sum(100))
+
+// This is the only way I understand how this works. I know I wasn't supposed to use a Function. 
